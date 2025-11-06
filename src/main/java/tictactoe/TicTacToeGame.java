4bc52146228b9eea17791f2cffbbeff8f37c5fb8
Philipp Schmitz <philipp.schmitz3@alumni.fh-aachen.de>
2025-11-06 21:25:19 +0100

    fixed toString

--- src/main/java/tictactoe/TicTacToeGame.java
+++ src/main/java/tictactoe/TicTacToeGame.java
@@ -97,14 +97,6 @@ public class TicTacToeGame {
 
     @Override
     public String toString() {
-        /*return
-        "\n" + board[0][0] + "  |  " + board[0][1] + "  |  " + board[0][2] +
-        "\n-------------" +
-        "\n" + board[1][0] + "  |  " + board[1][1] + "  |  " + board[1][2] +
-        "\n-------------" +
-        "\n" + board[2][0] + "  |  " + board[2][1] + "  |  " + board[2][2];
-        */
-
         return  "\n   A     B     C\n" +
                 "      |     |     \n" +
                 "1  " + board[0][0] + "  |  " + board[1][0] +  "  |  " + board[2][0] + "  \n" +
